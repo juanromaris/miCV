498e433dac16e5c0af3e966b74b6dcd24af3657f
Juan <juan_romaris@hotmail.com>
2017-06-10 14:56:46 -0300

    app

---
+++ js/app.js
@@ -0,0 +1,44 @@
+var bio = {
+
+	nombre: "Juan",
+ 	apellido: "Romaris",
+ 	pais:"Arg",
+ 	edad:"27",
+ 	telefono: "(011) 15-3574-2022",
+ 	linkGitHub: "https://github.com/juanromaris",
+	mostrar: function () {console.log("BIO")},
+
+};
+
+
+var experiencia = {
+
+	cargo: "Diseñador Gráfico"
+	empresa: "Colombraro",
+ 	desde: "01/2015",
+ 	hasta:"Actualidad",
+ 	mostrar: function () {console.log("experiencia")};
+	  
+
+};
+
+
+
+var educacion = {
+
+	diploma: "Diseñador Gráfico",
+ 	universidad: "UBA",
+ 	pais:"Arg",
+ 	mostrar: function () {console.log("educacion")};
+	  
+
+};
+
+
+var proyectos = {
+
+	proyecto: "calculadora",
+ 	mostrar: function () {console.log("proyectos")};
+
+ 	
+};
